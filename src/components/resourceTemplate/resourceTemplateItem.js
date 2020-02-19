d1a8a8eb448a5a58fde5f66cdf40dd08cb8314c9
MarianDutchyn <marian.dutchyn@ukr.net>
2020-02-19 20:52:41 +0200

    fix resource template list

--- src/components/resourceTemplate/resourceTemplateItem.js
+++ src/components/resourceTemplate/resourceTemplateItem.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
+const style = {
+    maxWidth: 345,
+    minWidth: 300,
+    margin: 10,
+
+}
+
 class ResourceTemplateItem extends Component{
+
 
     state = {
         id:	this.props.id,
@@ -14,9 +28,20 @@ class ResourceTemplateItem extends Component{
     
     render(){
         return(
-            <div>
-                {this.state.name}
-            </div>
+            <Link to={`/resource/update/${this.state.id}`} >
+            <Card style={style}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {this.state.name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {this.state.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
         );
     }
 }
