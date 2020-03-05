7a795c935161ac9bb496ebd39e231d1b3b59f635
sydor_artur <sydor.artur@ukr.net>
2020-03-05 12:21:14 +0200

    add pagination for groups

--- src/components/group/groupList.js
+++ src/components/group/groupList.js
@@ -4,6 +4,8 @@ import MaterialTable from "material-table";
 import {Box, Button, Grid} from "@material-ui/core";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import Alert from "@material-ui/lab/Alert";
+import CustomPagination from "./customPagination";
+
 
 const columns = [
     {title: 'Name', field: 'name'},
@@ -14,24 +16,42 @@ const gridStale = {
     marginBottom: 20
 };
 
+const paginationStyle = {
+    padding: 20
+};
+
+const itemsNumber = 5;
+
 const noError = '';
 
 class GroupList extends Component {
     state = {
         groups: [],
+        activePage: 1,
+        totalPages: 0,
+        itemsCountPerPage: 0,
+        totalItemsCount: 0,
         errorMessage: "",
     };
 
-    getData = () => {
-        axios.get("group").then(response => {
-            let groups = response.data;
-            this.setState({groups});
+
+    getData = (pageNumber) => {
+        axios.get(`group?page=${pageNumber}&size=${itemsNumber}`).then(response => {
+            let groups = response.data.content;
+            let totalPages = response.data.totalPages;
+            let itemsCountPerPage = response.data.numberOfElements;
+            let totalItemsCount = response.data.totalElements;
+            this.setState({
+                groups: groups,
+                totalPages: totalPages,
+                itemsCountPerPage: itemsCountPerPage,
+                totalItemsCount: totalItemsCount
+            });
         });
-        this.state.groups.reverse();
     };
 
     componentDidMount() {
-        this.getData();
+        this.getData(this.state.activePage);
     }
 
     goToEditGroup(name) {
@@ -40,6 +60,15 @@ class GroupList extends Component {
 
     goBack = () => {
         this.props.history.push("/home");
+    };
+
+    handlePageChange = (event, pageNumber) => {
+        this.setState({activePage: pageNumber});
+        this.getData(pageNumber)
+    };
+
+    handleDeleteItem = () => {
+        return this.state.totalItemsCount % 5 === 0;
     };
 
     render() {
@@ -76,13 +105,12 @@ class GroupList extends Component {
                         onRowAdd: newData => {
                             axios.post("group", newData).then(response => {
                                 this.setState({errorMessage: noError});
-                                this.props.history.push("/group");
                             }, error => {
                                 this.setState({errorMessage: error.response.data.message});
                             });
                             return new Promise(resolve => {
                                 setTimeout(() => {
-                                    this.getData();
+                                    this.getData(this.state.activePage);
                                     resolve();
                                 }, 600);
                             })
@@ -90,7 +118,12 @@ class GroupList extends Component {
                         onRowDelete: oldData => {
                             axios.delete(`group/${oldData.name}`).then(
                                 response => {
-                                    this.setState({errorMessage: noError})
+                                    this.setState({errorMessage: noError,
+                                        totalItemsCount: (this.state.totalItemsCount - 1)});
+                                    if(this.handleDeleteItem()) {
+                                        let newActivePage = (this.state.activePage - 1);
+                                        this.setState({activePage: newActivePage})
+                                    }
                                 },
                                 error => {
                                     this.setState({errorMessage: error.response.data.message});
@@ -98,13 +131,29 @@ class GroupList extends Component {
                             );
                             return new Promise(resolve => {
                                 setTimeout(() => {
-                                    this.getData();
+                                    this.getData(this.state.activePage);
                                     resolve();
                                 }, 600);
                             })
                         }
                     }}
+                    options={{
+                        actionsColumnIndex: -1,
+                        paging: false,
+                        search: false
+                    }}
                 />
+                <Grid container
+                      style={paginationStyle}
+                      justify="center">
+                    <CustomPagination
+                        activepage={this.state.activePage}
+                        totalPages={this.state.totalPages}
+                        itemsCountPerPage={this.state.itemsCountPerPage}
+                        totalItemsCount={this.state.totalItemsCount}
+                        onChange={this.handlePageChange}
+                    />
+                </Grid>
             </div>
         )
     }
