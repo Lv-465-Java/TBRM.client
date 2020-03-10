6e72938fcb2bf48bdba55f4622106a4d8ca6e73d
Andrii Bren <43859320+billyxd3@users.noreply.github.com>
2020-03-10 14:49:31 +0200

    Lvjv google maps (#24)
    * Add google maps implementation, ResourceRecordItemView page
    
    * Implement google maps api, Add ResourceRecordCreate
    
    * Create PointInteger parameter type component
    
    * Add RangeDouble, PointString, PointDouble parameter types
    
    * Create RangeInteger, ResourceRecordUpdate, add delete, update methods
    
    * Add ability to show specific related resource
    
    * Add PointReference, ResourceRecordDropDown, related resource record list
    
    * Add reference to related resource record in sub resource records table
    
    * Update Point Reference
    
    * Resolve conflicts, delete underliend links

---
+++ src/components/resourceRecord/ResourceRecordUpdate.js
@@ -0,0 +1,145 @@
+import React, {Component} from 'react';
+import axios from "../../utils/axios";
+import DialogContent from "@material-ui/core/DialogContent";
+import {FormControl, TextField} from "@material-ui/core";
+import PointInteger from "./parametersTypes/PointInteger";
+import PointString from "./parametersTypes/PointString";
+import PointDouble from "./parametersTypes/PointDouble";
+import RangeInteger from "./parametersTypes/RangeInteger";
+import RangeDouble from "./parametersTypes/RangeDouble";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import PointReference from "./parametersTypes/PointReference";
+
+class ResourceRecordUpdate extends Component {
+
+    state = {
+        name: undefined,
+        description: undefined,
+        resourceParameters: this.props.resourceTemplate.resourceParameters,
+        parameters: {}
+    }
+
+    update = () => {
+        axios.patch(`/resource-template/resource/${this.props.tableName}/${this.props.item.id}`, this.state).then(
+            response => {
+                this.setState({
+                    name: "",
+                    description: "",
+                    parameters: {},
+                    // data: {}
+                })
+                this.props.getRecordsData()
+            }).catch(error => {
+            console.dir(error.response.data);
+        })
+    };
+
+    onChangeName = (event) => {
+        let name = event.target.value;
+        if (name.trim().length === 0) {
+            name = undefined;
+        }
+        this.setState({name});
+    }
+
+    onChangeDescription = (event) => {
+        let description = event.target.value;
+        this.setState({description});
+
+        // this.props.setData(this.props.columnName, event.target.value)
+    }
+
+    setData = (columnName, value) => {
+        this.setState({parameters: {...this.state.parameters, [columnName]: value}})
+    }
+
+    render() {
+        return (
+            <div>
+                <DialogContent dividers>
+                    <div>
+                        <FormControl>
+                            <TextField required type="text" label="name" onChange={this.onChangeName}/>
+                            {/*<TextField required type="text" label="name" value={this.props.item.name} onChange={this.onChangeName}/>*/}
+                            {/*// helperText={this.state.errorMessage} error={!!this.state.errorMessage}/>*/}
+
+                        </FormControl>
+                    </div>
+                    <div>
+                        <FormControl>
+                            <TextField type="text" label="description" onChange={this.onChangeDescription}/>
+                            {/*<TextField type="text" label="description" value={this.props.item.description} onChange={this.onChangeDescription}/>*/}
+                        </FormControl>
+                    </div>
+                    {
+                        // elements
+                        this.state.resourceParameters.map(element => {
+                            let e;
+                            if (element.parameterType === 'POINT_INT') {
+                                e = (<PointInteger key={element.name}
+                                                   label={element.name}
+                                                   columnName={element.columnName}
+                                                   value={this.props.item.parameters[element.columnName]}
+                                                   setData={this.setData}/>)
+                            } else if (element.parameterType === 'POINT_STRING') {
+                                e = (<PointString key={element.name}
+                                                  label={element.name}
+                                                  columnName={element.columnName}
+                                                  value={this.props.item.parameters[element.columnName]}
+                                                  setData={this.setData}/>)
+                            } else if (element.parameterType === 'POINT_DOUBLE') {
+                                e = (<PointDouble key={element.name}
+                                                  label={element.name}
+                                                  columnName={element.columnName}
+                                                  value={this.props.item.parameters[element.columnName]}
+                                                  setData={this.setData}/>)
+                            } else if (element.parameterType === 'RANGE_INTEGER') {
+                                e = (<RangeInteger key={element.name}
+                                                   label={element.name}
+                                                   columnName={element.columnName}
+                                                   value={this.props.item.parameters[element.columnName]}
+                                                   setData={this.setData}/>)
+                            } else if (element.parameterType === 'RANGE_DOUBLE') {
+                                e = (<RangeDouble key={element.name}
+                                                  label={element.name}
+                                                  columnName={element.columnName}
+                                                  value={this.props.item.parameters[element.columnName]}
+                                                  setData={this.setData}/>)
+                            } else if (element.parameterType === 'POINT_REFERENCE') {
+                                e = (<PointReference key={element.name}
+                                                     label={element.name}
+                                                     columnName={element.columnName}
+                                                     relatedResourceTableName={element['relatedResourceTemplateTableName']}
+                                                     setData={this.setData}/>)
+                            }
+                            return e;
+                        })
+                    }
+                    {/*//     (<FormControl>*/}
+                    {/*//*/}
+                    {/*//             {*/}
+                    {/*//                 if(element.parameterType === 'POINT_INT'){*/}
+                    {/*//                 <TextField key={element.name}*/}
+                    {/*//                 type="text"*/}
+                    {/*//                 label={element.name}*/}
+                    {/*//                 setData={this.setData}/>}*/}
+                    {/*//             }*/}
+                    {/*//         </FormControl>)*/}
+                    {/*//*/}
+                    {/*// )}*/}
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={this.update} color="primary">
+                        Update
+                    </Button>
+                    <Button autoFocus onClick={this.props.handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </div>
+        );
+    }
+}
+
+export default ResourceRecordUpdate;
--- src/components/resourceRecord/ResourceRecordView.js
+++ src/components/resourceRecord/ResourceRecordView.js
@@ -4,26 +4,40 @@ import axios from "../../utils/axios";
 import ResourceRecordList from "./ResourceRecordList";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ResourceRecordCreate from "./ResourceRecordCreate";
+
+// const gridStyles = {
+//     marginLeft: 300
+// }
 
 class ResourceRecordView extends Component {
 
     state = {
         records: [],
-        resourceTemplate: undefined,
-
+        resourceTemplate: "",
+        tableName: this.props.match.params.tableName,
+        openDialog: false
     }
+
     getRecordsData = () => {
-        let tableName = this.props.match.params.tableName;
-        axios.get(`/resource-template/resource/${tableName}`).then(response => {
+        axios.get(`/resource-template/resource/${this.state.tableName}`).then(response => {
             this.setState({records: response.data})
         })
     }
 
     getResourceTemplateData = () => {
-        let tableName = this.props.match.params.tableName;
-        axios.get(`/resource-template/table/${tableName}`).then(response => {
+        axios.get(`/resource-template/table/${this.state.tableName}`).then(response => {
             this.setState({resourceTemplate: response.data})
         })
+    }
+    handleClose = () => {
+        this.setState({openDialog: false})
+    }
+    handleOpen = () => {
+        this.setState({openDialog: true})
     }
 
     componentDidMount() {
@@ -31,21 +45,53 @@ class ResourceRecordView extends Component {
         this.getResourceTemplateData();
     }
 
+
+
     render() {
+        // function relatedResourceTableName() {
+        //     this.state.resourceTemplate.resourceParameters.map(key => {
+        //         if (key.parameterType === "POINT_REFERENCE") {
+        //             return key['relatedResourceTemplateTableName'];
+        //         }
+        //     })
+        // }
         return (
             <div>
+                <div>
+                    <h1>{this.state.resourceTemplate.name}</h1>
+                </div>
                 <Button
                     variant="contained"
                     color="primary"
-                    startIcon={<CheckCircleIcon/>}>
+                    startIcon={<CheckCircleIcon/>}
+                    onClick={this.handleOpen}>
                     Add record
                 </Button>
-                {this.state.resourceTemplate &&
-                <ResourceRecordList
-                    records={this.state.records}
-                    resourceTemplate={this.state.resourceTemplate}
-                    getRecordData={this.getRecordsData}
-                />}
+
+                <Grid container spacing={3}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={8}>
+                        {this.state.resourceTemplate &&
+                        <ResourceRecordList
+                            tableName={this.state.tableName}
+                            records={this.state.records}
+                            resourceTemplate={this.state.resourceTemplate}
+                            getRecordsData={this.getRecordsData}
+                        />}
+                    </Grid>
+                    <Grid item xs={3}></Grid>
+                </Grid>
+                <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.openDialog}>
+                    <DialogTitle id="simple-dialog-title">Create new {this.state.resourceTemplate.name}</DialogTitle>
+
+                    <ResourceRecordCreate handleClose={this.handleClose}
+                                          tableName={this.state.tableName}
+                                          resourceTemplate={this.state.resourceTemplate}
+                                          // relatedResourceTableName={relatedResourceTableName}
+                                          getRecordsData={this.getRecordsData}
+                    />
+
+                </Dialog>
             </div>
         );
     }
